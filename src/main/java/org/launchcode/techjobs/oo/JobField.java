5cd892c068450b31a903c5f7f2507a4a48a58923
strickte <166240135+strickte@users.noreply.github.com>
2024-11-18 18:34:02 -0600

    Thank you KJ

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -46,8 +46,8 @@ public abstract class JobField {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof CoreCompetency)) return false;
-        CoreCompetency that = (CoreCompetency) o;
+        if (!(o instanceof JobField)) return false;
+        JobField that = (JobField) o;
         return id == that.getId();
     }
 
